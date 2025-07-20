385ff9d3ecf42673ebd509e85d7a586b8727e8e2
Abin Alex Pothen <abinalexpothen@gmail.com>
2025-07-20 00:20:50 -0400

    Learn a few things on string init, cin incorrect input result.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 int main()
 {
-  std::cout << "Hello, world!\n";
+  std::cout << "Enter your first name, second name and age in years: ";
+  std::string first_name;
+  std::string second_name;
+  int age = -1;
+  std::cin >> first_name >> second_name >> age;
+  std::cout << "Hello, " << first_name << " " << second_name << "!\n";
+  std::cout << "You are " << age * 12 << " months old.\n";
+
   return 0;
 }
