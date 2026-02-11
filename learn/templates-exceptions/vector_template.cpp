05ad67e8e29c31c299e10924edf9a58a68f3a352
Abin Alex Pothen <abinalexpothen@gmail.com>
2026-02-11 07:21:28 -0500

    Define Vector with template parameter T and a value template parameter A containing an allocator member function

--- learn/templates-exceptions/vector_template.cpp
+++ learn/templates-exceptions/vector_template.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-template <typename T>  // for all types T. Also called type generator
+template <typename T>
+class allocator {
+ public:
+  T* allocate(int n);            // allocate  space for n objects of type T
+  void deallocate(T* p, int n);  // deallocate n objects of type T starting at p
+};
+
+template <typename T, typename A = allocator<T>>  // for all types T. Also called type generator
 class Vector {
+  A alloc;    // member function allocate handles memory
   int sz;     // the size
   T* elem;    // a pointer to the elements
   int space;  // size + free space
@@ -32,7 +40,12 @@ class Vector {
   int size() const { return sz; }
   int capacity() const { return space; }
 
-  void resize(int newsize);
+  void resize(int sz, T def = T{}) {
+    std::cout << "Resizing";
+
+    // resizing algorithm
+  }
+
   void push_back(const T& d);
   void reserve(int newalloc);
 
@@ -46,7 +59,38 @@ bool operator==(const Vector<T>&, const Vector<T>&);
 template <typename T>
 bool operator!=(const Vector<T>&, const Vector<T>&);
 
+// template <typename T, typename A = allocator<T>>
+// void Vector<T, A>::resize(int sz, T def) {
+//   std::cout << "sz is: " << sz;
+// }
+
+// struct TestStruct {
+//   int a;
+//   double b;
+//   std::string c;
+//   TestStruct() : a{0}, b{0.0}, c{""} {}
+// };
+
+class No_default {
+ public:
+  No_default(int num)  // the only constructor
+  {
+    std::cout << "No fault number is: " << num;
+  }
+};
+
 int main() {
-  // Add stuff to test template class once the functions are implemented
+  // generalizing vectors
+  Vector<double> v1;
+  v1.resize(100);       // add 100 copied of double{}
+  v1.resize(200, 0.0);  // add 100 copies of 0.0
+  v1.resize(300, 1.0);  // add 100 copies of 1.0
+
+  // Vector<No_default> v2(10);  // error: no No_default()
+  Vector<No_default> v3;          // ok: no elements
+  v3.resize(100, No_default{2});  // ok: not calling default constructor No_default()
+  // v3.resize(200);  // error: no No_default()
+
+  // allocators
   return 0;
 }
